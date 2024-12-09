83ae816bde7f5cd26d870eed73007ce7b4047fea
«Elena <LenaRybinskova@gmail.com>
2024-12-09 15:41:54 +0300

    add Layout component

--- pages/characters/index.tsx
+++ pages/characters/index.tsx
@@ -1,24 +1,24 @@
 import {useCharacters} from '@/assets/hooks/useCharacters';
 import {CharacterCard} from '@/components/CharacterCard/CharacterCard';
-import styles from '@/styles/Home.module.css';
 import {HeadMeta} from '@/components/HeadMeta/HeadMeta';
-import {Navbar} from '@/components/Navbar/Navbar';
-import {Inter} from 'next/dist/compiled/@next/font/dist/google';
+import {getLayout} from '@/components/Layout/Layout';
 
-// const inter = Inter({subsets:["latin"]})
 
-export default function Characters() {
+function Characters() {
     const characters = useCharacters()
 
     return (
         <>
             <HeadMeta title={'Characters'}/>
-            <main className={`${styles.main}`}>
-                <Navbar/>
+{/*            <main className={`${styles.main}`}>*/}
+{/*                <Navbar/>*/}
                 {characters && characters.map(char => (
                     <CharacterCard key={char.id} char={char}/>
                 ))}
-            </main>
+{/*            </main>*/}
         </>
     );
 }
+
+Characters.getLayout=getLayout
+export default Characters;
