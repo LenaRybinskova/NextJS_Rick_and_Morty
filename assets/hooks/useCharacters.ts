9b28ad2ed46e757c08f3a3948d9d37736235432f
«Elena <LenaRybinskova@gmail.com>
2024-12-09 16:53:36 +0300

    add .env.local

--- assets/hooks/useCharacters.ts
+++ assets/hooks/useCharacters.ts
@@ -1,6 +1,6 @@
 import {useEffect, useState} from 'react';
 import axios from 'axios';
-import {Nullable} from '@/assets/hooks/types';
+import {Nullable} from 'assets/hooks/types';
 
 
 export type Character ={
@@ -13,7 +13,7 @@ export const useCharacters=():Nullable<Character[]>=>{
     const [characters, setCharacters]=useState<Nullable<Character[]>>(null)
 
     useEffect(()=>{
-        axios.get("https://rickandmortyapi.com/api/character").then(res =>setCharacters(res.data.results) )
+        axios.get(`${process.env.NEXT_PUBLIC_RICK_AND_MORTY_API_URL}/character`).then(res =>setCharacters(res.data.results) )
     }, [])
 
     return characters;
